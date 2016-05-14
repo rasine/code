3cb1a4302112e9b8375e5abb5dd02805a48ee5ce
rasine_fly <rasine_cyf@qq.com>
2016-05-14 17:53:29 +0800

    调整

---
+++ python/python.py
@@ -0,0 +1,37 @@
+# -*- coding=UTF-8 -*-
+import sys
+
+#显示中文
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+class ClassTest(object):
+	"""docstring for ClassTest"""
+	data = ["value1","value2","value3"]
+	x = 7
+
+	def __init__(self,name,color):
+		self.name = name
+		self.color = color
+
+	def sayHello(self):
+		print "%s hello" %(self.name)
+
+	def getName(self):
+		return self.name
+
+	def getColor(self):
+		print self.color
+
+test = ClassTest("rasine","red")
+print ClassTest.x
+print test.x
+test.x += 1
+print ClassTest.x
+print test.x
+del test.x
+print ClassTest.x
+print test.x
+print dir(test)
+print dir(ClassTest)
+print globals()
